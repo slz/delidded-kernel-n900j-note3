09230d8b27c174a776690cd994f9c7fa97e37c2b
slz <madskiilz@yahoo.com>
2015-11-17 11:01:59 -0500

    build scripts: Added to project

---
+++ dl-build-ramdisk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. ./dl-build.inc
+
+rm -rf build/ramdisk
+
+if BUILD_RAMDISK && BUILD_BOOT_IMG; then
+	if [ $MAKE_ZIP -eq 1 ]; then CREATE_ZIP; fi
+	if [ $MAKE_TAR -eq 1 ]; then CREATE_TAR; fi
+	echo "Finished!"
+else
+	echo "Error!"
+	exit -1
+fi
